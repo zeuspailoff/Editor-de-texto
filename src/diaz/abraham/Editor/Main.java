ce54cfa3cb20b964249239bfadbef8ffcfc749bb
Abraham Díaz <114292541+zeuspailoff@users.noreply.github.com>
2023-01-26 15:50:43 +0000

    creamos ventana con diferentes clases

---
+++ src/diaz/abraham/Editor/Main.java
@@ -0,0 +1,12 @@
+package diaz.abraham.Editor;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Ventana borde = new Ventana();
+        borde.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        borde.setVisible(true);
+
+    }
+}
---
+++ src/diaz/abraham/Editor/Ventana.java
@@ -0,0 +1,16 @@
+package diaz.abraham.Editor;
+
+import javax.swing.*;
+
+public class Ventana extends JFrame {
+    public Ventana() {
+        setBounds(300,300,600,600);
+        setTitle("ThunderEdit");
+    }
+}
+
+class Panel extends JPanel {
+    public Panel(){
+
+    }
+}
