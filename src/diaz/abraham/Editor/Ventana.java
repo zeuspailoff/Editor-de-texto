71d3ae4472d227bfb5b5e3a495bf9002d66782cd
Abraham Díaz <114292541+zeuspailoff@users.noreply.github.com>
2023-01-27 13:28:27 +0000

    añadimos funcionalidad con arraylist

--- src/diaz/abraham/Editor/Ventana.java
+++ src/diaz/abraham/Editor/Ventana.java
@@ -3,6 +3,8 @@ package diaz.abraham.Editor;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
 
 //-------------------- creamos la ventana del programa
 public class Ventana extends JFrame {
@@ -53,6 +55,9 @@ class Panel extends JPanel {
         panelMenu.add(menu);
         //--------------------area de texto------------------------
         tPane = new JTabbedPane();
+        listFile = new ArrayList<File>();
+        listAreaTexto = new ArrayList<JTextPane>();
+        listAreaScroll = new ArrayList<JScrollPane>();
         //--------------------añadimos los objetos a la vista del programa----------------
 
         add(panelMenu);
@@ -104,15 +109,29 @@ class Panel extends JPanel {
     }
     //-------------------- creamos las ventanas de texto------------------------
     public void creaPanel(){
-        ventanaText = new JPanel();
-        areaText = new JTextPane();
 
-        ventanaText.add(areaText);
+
+        ventanaText = new JPanel();
+
+        listFile.add(new File(""));
+        listAreaTexto.add(new JTextPane());
+        listAreaScroll.add(new JScrollPane(listAreaTexto.get(contadorVentana)));
+
+
+        ventanaText.add(listAreaScroll.get(contadorVentana));
         tPane.addTab("nueva ventana",ventanaText);
+        contadorVentana++;
+        existeVentana = true;
     }
+    private int contadorVentana = 0;//nos cuenta las ventanas de texto
+    private boolean existeVentana = false;// comprobamos si exiten ventanas creadas
     private JTabbedPane tPane;
     private  JPanel ventanaText;
-    private JTextPane areaText;
+    //private JTextPane areaText;
+    private ArrayList<JTextPane> listAreaTexto;
+    private ArrayList<JScrollPane> listAreaScroll;
+    private ArrayList<File> listFile;
+
     //-------------------- creamos opciones del menu----------------
     private JMenuBar menu;
     private JMenu archivo, editar, seleccion, ver, apariencia;
