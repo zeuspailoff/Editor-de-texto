3324f5d629ea746be65dac33900b91ac00a005c3
Abraham Díaz <114292541+zeuspailoff@users.noreply.github.com>
2023-01-27 11:56:14 +0000

    creamos ventana con diferentes clases

--- src/diaz/abraham/Editor/Ventana.java
+++ src/diaz/abraham/Editor/Ventana.java
@@ -12,10 +12,37 @@ public class Ventana extends JFrame {
 
 class Panel extends JPanel {
     public Panel(){
+        //--------------------menu -----
+        JPanel panelMenu = new JPanel();
+        menu = new JMenuBar();
+        archivo = new JMenu("Archivo");
+        editar = new JMenu("Editar");
+        seleccion = new JMenu("Seleccion");
+        ver = new JMenu("Ver");
+        apariencia = new JMenu("Apariencia");
+        // creamos las opciones del menu
+        menu.add(archivo);
+        menu.add(editar);
+        menu.add(seleccion);
+        menu.add(ver);
+        menu.add(apariencia);
+
+        JMenuItem nuevo = new JMenuItem("Nuevo Archivo");
+        JMenuItem abrir = new JMenuItem("Abrir Archivo");
+        JMenuItem guardar = new JMenuItem("Guardar");
+        JMenuItem guardarC = new JMenuItem("Guardar Como");
+
+        archivo.add(nuevo);
+        archivo.add(abrir);
+        archivo.add(guardar);
+        archivo.add(guardarC);
+
+        panelMenu.add(menu);
         //--------------------area de texto------------------------
         tPane = new JTabbedPane();
-        //-------------------- fin de area de texto----------------
+        //--------------------añadimos los objetos a la vista del programa----------------
         creaPanel();
+        add(panelMenu);
         add(tPane);
     }
     //-------------------- creamos las ventanas de texto------------------------
@@ -30,4 +57,6 @@ class Panel extends JPanel {
     private  JPanel ventanaText;
     private JTextPane areaText;
     //-------------------- fin de la creacion ventanas de texto----------------
+    private JMenuBar menu;
+    private JMenu archivo, editar, seleccion, ver, apariencia;
 }
